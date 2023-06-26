1e26e4d6f40f34a9b35818a61b0b2712d6051d86
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-06-26 20:59:57 +0300

    Create LogicNotPushAdvanced.pl

---
+++ Задание 1/LogicNotPushAdvanced.pl
@@ -0,0 +1,89 @@
+push(and(X, 0), 0) :- !.
+push(and(0, X), 0) :- !.
+push(and(X, 1), Z) :- 
+    push(X, Z),
+    !.
+push(and(1, X), Z) :- 
+    push(X, Z),
+    !.
+push(and(X, Y), and(U, V)) :- 
+    push(X, U), 
+    push(Y, V), 
+    !.
+push(or(X, 0), Z) :- 
+    push(X, Z), 
+    !.
+push(or(0, X), Z) :- 
+    push(X, Z), 
+    !.
+push(or(X, 1), 1) :- !.
+push(or(1, X), 1) :- !.
+push(or(X, Y), or(U, V)) :- 
+    push(X, U), 
+    push(Y, V),
+    !.
+push(not(0), 1) :- !.
+push(not(1), 0) :- !.
+push(not(and(X, Y)), or(U, V)) :- 
+    push(not(X), U), 
+    push(not(Y), V), 
+    !.
+push(not(or(X, Y)), and(U, V)) :- 
+    push(not(X), U), 
+    push(not(Y), V), 
+    !.
+push(not(not(X)), Z) :- 
+    push(X, Z).
+push(X, X).
+
+simplify(or(1, X), 1) :- !.
+simplify(or(X, 1), 1) :- !.
+simplify(or(0, X), Z) :-
+    simplify(X, Z).
+simplify(or(X, 0), Z) :-
+    simplify(X, Z).
+simplify(or(X, Y), Z) :- 
+    simplify(X, U), 
+    simplify(Y, V), 
+    \+ number(U),
+    \+ number(V),
+    Z is or(U, V),
+    !.
+simplify(or(X, Y), Z) :-
+    simplify(X, U),
+    simplify(Y, V),
+    simplify(or(U, V), Z).
+simplify(and(0, X), 0) :- !.
+simplify(and(X, 0), 0) :- !.
+simplify(and(1, X), Z) :- 
+    simplify(X, Z),
+    !.
+simplify(and(X, 1), Z) :- 
+    simplify(X, Z),
+    !.
+simplify(and(X, Y), Z) :-
+    simplify(X, U),
+    simplify(Y, V),
+    \+ number(U),
+    \+ number(V),
+    Z = and(X, Y),
+    !.
+simplify(and(X, Y), Z) :-
+    simplify(X, U),
+    simplify(Y, V),
+    simplify(and(U, V), Z),
+    !.
+
+simplify(not(0), 1) :- !.
+simplify(not(1), 0) :- !.
+simplify(not(X), Z) :-
+    simplify(X, U),
+    \+ number(U),
+    Z is U,
+    !.
+simplify(not(X), Z) :-
+    simplify(X, U),
+    simplify(not(U), Z),
+    !.
+
+simplify(X, X).
