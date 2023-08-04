17c5500e6c82f27cc0c31a2c53e9d092c26c3e68
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-08-04 17:05:23 +0300

    Create cities.pl

---
+++ Game Cities/cities.pl
@@ -0,0 +1,27 @@
+getLast([X], X) :- !.
+getLast([X | Y], Last) :-
+    getLast(Y, Last).
+
+getFirst([X | Y], X) :- !.
+
+init([A | B]) :-
+    assert(city(A)),
+    init(B),
+    !.
+
+start(Current, Target, X) :-
+    atom_chars(Current, Current1),
+    getLast(Current1, Last),
+    Last = Target,
+    X = [],
+    !.
+start(Current, Target, [X | Y]) :-
+    city(City),
+    atom_chars(City, City1),
+    atom_chars(Current, Current1),
+    getFirst(City1, First),
+    getLast(Current1, Last),
+    Last = First,
+    X = City,
+    start(City, Target, Y),
+    !.
